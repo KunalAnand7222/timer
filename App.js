a3c639a2aaec23e3ac116b3b3ff4b03241d41cfa
Kunal Anand <161216758+KunalAnand7222@users.noreply.github.com>
2025-01-25 14:51:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -65,77 +65,7 @@ function App() {
 }
 
 export default App
-// import React, { useState, useEffect } from 'react';
 
-// function App() {
-//   const [time, setTime] = useState(1500); // Default to 25 minutes
-//   const [run, setRun] = useState(false);
-//   const [work, setWork] = useState(25);
-//   const [breakk, setBreak] = useState(5);
-//   const [isWork, setIsWork] = useState(true);
 
-//   useEffect(() => {
-//     let timer;
-//     if (run) {
-//       timer = setInterval(() => {
-//         setTime(prevTime => {
-//           if (prevTime <= 0) {
-//             if (isWork) {
-//               alert('Work period complete! Time for a break.');
-//             } else {
-//               alert('Break period complete! Back to work.');
-//             }
-//             setIsWork(!isWork);
-//             return isWork ? breakk * 60 : work * 60;
-//           }
-//           return prevTime - 1;
-//         });
-//       }, 1000);
-//     }
-//     return () => clearInterval(timer);
-//   }, [run, isWork, work, breakk]);
-
-//   const handleStart = () => setRun(true);
-//   const handleStop = () => setRun(false);
-//   const handleReset = () => {
-//     setRun(false);
-//     setTime(work * 60);
-//     setIsWork(true);
-//   };
-
-//   const handleSet = () => {
-//     setTime(work * 60);
-//     setIsWork(true);
-//   };
-
-//   const formatTime = (seconds) => {
-//     const m = Math.floor(seconds / 60);
-//     const s = seconds % 60;
-//     return `${m}:${s < 10 ? '0' : ''}${s}`;
-//   };
-
-//   return (
-//     <div className="hello">
-//       <h1>{formatTime(time)}</h1>
-//       <h1>{isWork ? 'Work - Time' : 'Break - Time'}</h1>
-//       <button onClick={handleStart} disabled={run}>Start</button>
-//       <button onClick={handleStop} disabled={!run}>Stop</button>
-//       <button onClick={handleReset}>Reset</button><br/>
-//       <input
-//         type="number"
-//         placeholder='Enter work time'
-//         value={work}
-//         onChange={(e) => setWork(Number(e.target.value))}
-//       />
-//       <input
-//         type="number"
-//         placeholder='Enter break time'
-//         value={breakk}
-//         onChange={(e) => setBreak(Number(e.target.value))}
-//       />
-//       <button onClick={handleSet}>Set</button>
-//     </div>
-//   );
-// }
 
 // export default App;
